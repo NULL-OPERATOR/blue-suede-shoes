86fad9de5354e9ae4e250900c13b46f39c29d7aa
NULL-OPERATOR <x@cryptolab.net>
2016-02-03 01:26:19 +0000

    can remove items from cart

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,16 +1,60 @@
 'use strict';
 angular.module('app.controllers', [])
 
-.controller('appCtrl', ['shopItems', function(shopItems) {
-    this.cart = [];
-    this.items = shopItems.query();
+.controller('appCtrl', ['shopItems', "$filter", function(shopItems, $filter) {
+  this.cart = [];
+  this.items = shopItems.query();
 
-  this.reduceQuantity = function(item) {
-    this.items[item.id]["quantity"] -= 1;
+  this.reduceStock = function(item) {
+    this.items[item.id].quantity --;
+  };
+
+  this.increaseStock = function(item) {
+    this.items[item.id].quantity ++;
+  };
+
+  this.checkCart = function(item) {
+    for(var i = 0; i < this.cart.length; i ++) {
+      if(this.cart[i].name === item.name) {
+        return i;
+      }
+    }
+  };
+
+  this.addItem = function(item) {
+    var index = this.checkCart(item)
+    this.reduceStock(item);
+    if (index === undefined) {
+      this.cart.push({
+        "name": item.name,
+        "quantity": 1
+      });
+    } else {
+      this.cart[index].quantity ++
+    }
+  };
+
+  this.cleanCart = function() {
+    for (var i = this.cart.length - 1; i >= 0; i--) {
+      if (this.cart[i] === undefined) {
+        this.cart.splice(i, 1);
+      }
+    }
   }
 
-  this.addToCart = function(item) {
-    this.reduceQuantity(item);
-    this.cart.push(item);
+  this.removeCartItem = function(index) {
+    delete this.cart[index]
+    this.cleanCart()
   };
+
+  this.removeItem = function(item) {
+    var index = this.checkCart(item)
+    this.increaseStock(item)
+    if (this.cart[index].quantity === 1) {
+      this.removeCartItem(index);
+    } else {
+      this.cart[index].quantity --;
+    }
+  }
+
 }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,18 +2,16 @@
 describe('blueSeudeApp controllers', function(){
 
   beforeEach(function(){
-       jasmine.addMatchers({
-           toEqualData: function(util, customEqualityTesters) {
-               return {
-                   compare: function(actual, expected) {
-                       return {
-                           pass: angular.equals(actual, expected)
-                       };
-                   }
-               };
-           }
-       });
-   });
+    jasmine.addMatchers({
+      toEqualData: function(util, customEqualityTesters) {
+        return {
+          compare: function(actual, expected) {
+            return { pass: angular.equals(actual, expected) };
+          }
+        };
+      }
+    });
+  });
 
   beforeEach(module('blueSeudeApp'));
 
@@ -21,6 +19,8 @@ describe('blueSeudeApp controllers', function(){
     var ctrl, $httpBackend;
     var itemsIn = [{ id:0, name:"shoes", discount: 0, quantity: 5 }];
     var itemsOut = [{ id:0, name:"shoes", discount: 0, quantity: 4 }];
+    var basket = [{ "name":"shoes", "quantity":1 }];
+    var shoes = itemsIn[0];
 
     beforeEach(inject(function(_$httpBackend_, $controller) {
       $httpBackend = _$httpBackend_;
@@ -41,33 +41,23 @@ describe('blueSeudeApp controllers', function(){
     describe("adding to the cart", function() {
       beforeEach(function() {
         $httpBackend.flush();
-        var shoes = ctrl.items[0];
-        ctrl.addToCart(shoes);
+        ctrl.addItem(shoes);
       });
 
       it('should reduce the current stock of the item by 1', function() {
-        var shoes = ctrl.items[0];
+        // var shoes = ctrl.items[0];
         expect(ctrl.items[shoes.id].quantity).toEqualData(4);
       });
+
       it('should be able to add a product to the cart', function() {
-        expect(ctrl.cart).toEqualData(itemsOut);
+        expect(ctrl.cart).toEqualData(basket);
+      });
+
+      it('can remove a product from the cart', function() {
+        ctrl.removeItem(shoes)
+        expect(ctrl.cart).toEqualData([]);
       });
     });
 
-    // describe("removing from the cart", function() {
-    //   beforeEach(function() {
-    //     $httpBackend.flush();
-    //     var shoes = ctrl.items[0];
-    //     ctrl.addToCart(shoes);
-    //   });
-    //
-    //   it('should reduce the current stock of the item by 1', function() {
-    //     var shoes = ctrl.items[0];
-    //     expect(ctrl.items[shoes.id].quantity).toEqual(4);
-    //   });
-    //   it('should be able to add a product to the cart', function() {
-    //     expect(ctrl.cart).toEqual(itemsOut);
-    //   });
-    // });
   });
 });
