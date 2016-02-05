ebca11700c19f1b8364f84d94c1ade16f25ec672
NULL-OPERATOR <x@cryptolab.net>
2016-02-05 01:09:45 +0000

    test travis

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,27 +1,27 @@
 module.exports = function(config){
   config.set({
 
-    basePath : '../',
+    basePath : './',
 
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/js/**/*.js',
-      'test/unit/**/*.js'
+      'app/components/**/*.js',
+      'app/view*/**/*.js'
     ],
 
     autoWatch : true,
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome', 'Firefox'],
+    browsers : ['Chrome'],
 
     plugins : [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
-            'karma-jasmine'
+            'karma-jasmine',
+            'karma-junit-reporter'
             ],
 
     junitReporter : {
